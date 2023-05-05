900ab30b2034ba1aa12c2db6189ca1919d66aabf
JeanFelipe <ermelindo35@hotmail.com>
2023-05-04 22:18:39 -0300

    alteração

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 function logar(){
+    // Obtém os valores dos campos de login e senha a partir dos seus IDs
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
